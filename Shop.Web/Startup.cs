1425a8c7f0e3254422063a71f87d5efa45894ed3
LAURA MARITZA RESTREPO MU�OZ <laurarestrepo220579@correo.itm.edu.co>
2019-02-11 16:08:46 -0500

    Create database feeder (SeedDb)

--- Shop.Web/Startup.cs
+++ Shop.Web/Startup.cs
@@ -30,7 +30,10 @@ namespace Shop.Web
             {
                 cfg.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection"));
             });
- 
+
+            services.AddTransient<SeedDb>();
+
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
