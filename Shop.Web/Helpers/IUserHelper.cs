afd8589c320f3ab12aefd7c6d45012d0ca080cfd
LAURA MARITZA RESTREPO MU�OZ <laurarestrepo220579@correo.itm.edu.co>
2019-03-25 17:07:11 -0500

    Create User Management

--- Shop.Web/Helpers/IUserHelper.cs
+++ Shop.Web/Helpers/IUserHelper.cs
@@ -2,6 +2,7 @@
 
 namespace Shop.Web.Helpers
 {
+    using System.Collections.Generic;
     using System.Threading.Tasks;
     using Data.Entities;
     using Microsoft.AspNetCore.Identity;
@@ -39,6 +40,11 @@ namespace Shop.Web.Helpers
 
         Task<IdentityResult> ResetPasswordAsync(User user, string token, string password);
 
+        Task<List<User>> GetAllUsersAsync();
+
+        Task RemoveUserFromRoleAsync(User user, string roleName);
+
+        Task DeleteUserAsync(User user);
 
 
     }
